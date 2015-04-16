d4161f34bc8b6ec47ca124d4c2ac34a77cb160c2
Nino Ninux <nino.ninux@gmail.com>
2015-04-16 15:44:21 +0200

    added lab script

---
+++ ueb_t/lab1/labor1.m
@@ -0,0 +1,12 @@
+td = 20*(sqrt(1*10^-3*10*10^-9))
+fg = 1/(pi*sqrt(1E-3*10E-9))
+z0 = sqrt((1E-3)/(10E-9))
+rq = 50
+r1 = sqrt(z0*(z0-rq))
+r2 = rq*z0/r1
+a  = sqrt(z0/rq) + sqrt((z0/rq)-1)
+
+d  = 2.02/1.42
+dlog = 20*log10(d)
+
+
